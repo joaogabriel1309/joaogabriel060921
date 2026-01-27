9b9662936e31d570da1dc31f47c9c6c3643c5abe
joaogabriel1309 <gabriel.122.gm85@gmail.com>
2026-01-27 14:02:34 -0400

    feat: adiciona entidades JPA Artista e Album com relacionamento N:N

---
+++ src/main/java/com/seplag/musicapi/dominio/entidade/Album.java
@@ -0,0 +1,28 @@
+package com.seplag.musicapi.dominio.entidade;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "album")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String nome;
+
+    @ManyToMany(mappedBy = "albuns")
+    private List<Artista> artistas;
+}
